5cbb543793a639c23846c4a571179384ae2f099b
Richard Wilkes <wilkes@me.com>
2017-10-31 17:29:14 -0700

    Add comment noting what this is for

--- install.sh
+++ install.sh
@@ -1,5 +1,13 @@
 #!/bin/sh
 
+# This will install the various tools provided with this repo. The only
+# difference between running this and doing:
+#
+# go get -u github.com/richardwilkes/gokit/...
+#
+# is proper version numbers with build dates and git revisions will be
+# embedded into the resulting executables.
+
 ROOT=`pwd`
 
 cd $ROOT/cmdline/cmd/genversion
