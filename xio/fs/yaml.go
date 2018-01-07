7ea320b3a7199284483ccf49ba4d4418fb700657
Richard Wilkes <wilkes@me.com>
2018-01-07 10:59:32 -0800

    Add yaml helpers

---
+++ xio/fs/yaml.go
@@ -0,0 +1,37 @@
+package fs
+
+import (
+	"io"
+	"io/ioutil"
+
+	"github.com/richardwilkes/toolbox/errs"
+	"github.com/richardwilkes/toolbox/xio/fs/safe"
+
+	"gopkg.in/yaml.v2"
+)
+
+// LoadYAML data from the specified path.
+func LoadYAML(path string, data interface{}) error {
+	in, err := ioutil.ReadFile(path)
+	if err != nil {
+		return errs.Wrap(err)
+	}
+	if err = yaml.Unmarshal(in, data); err != nil {
+		return errs.Wrap(err)
+	}
+	return nil
+}
+
+// SaveYAML data to the specified path.
+func SaveYAML(path string, data interface{}, format bool) error {
+	out, err := yaml.Marshal(data)
+	if err != nil {
+		return errs.Wrap(err)
+	}
+	return safe.WriteFile(path, func(w io.Writer) error {
+		if _, err := w.Write(out); err != nil {
+			return errs.Wrap(err)
+		}
+		return nil
+	})
+}
---
+++ xio/fs/yaml_test.go
@@ -0,0 +1,30 @@
+package fs_test
+
+import (
+	"io/ioutil"
+	"os"
+	"testing"
+
+	"github.com/richardwilkes/toolbox/xio/fs"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func TestLoadSaveYAML(t *testing.T) {
+	type data struct {
+		Name  string
+		Count int
+	}
+	value := &data{
+		Name:  "Rich",
+		Count: 22,
+	}
+	f, err := ioutil.TempFile("", "yamltest")
+	require.NoError(t, err)
+	require.NoError(t, f.Close())
+	require.NoError(t, fs.SaveYAML(f.Name(), value, false))
+	var value2 data
+	require.NoError(t, fs.LoadYAML(f.Name(), &value2))
+	require.NoError(t, os.Remove(f.Name()))
+	assert.Equal(t, value, &value2)
+}
