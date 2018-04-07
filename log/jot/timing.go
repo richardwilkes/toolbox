91738f710375fe32884ff5229f5e231721d5334c
Richard Wilkes <wilkes@me.com>
2018-04-07 08:48:27 -0700

    Add EndWithMsg() and EndWithMsgf()

--- log/jot/timing.go
+++ log/jot/timing.go
@@ -5,16 +5,34 @@ import (
 	"time"
 )
 
-// Timing is used to record the duration between two events.
+// Timing is used to record the duration between two events. One of End(),
+// EndWithMsg(), or EndWithMsgf() should be called when the event has
+// finished.
 type Timing struct {
 	started time.Time
 	msg     string
 }
 
-// End should be called when the event has finished.
+// End finishes timing an event and logs an informational message.
 func (timing *Timing) End() time.Duration {
 	elapsed := time.Since(timing.started)
 	Infof("Finished %s | %v elapsed", timing.msg, elapsed)
+	return elapsed
+}
+
+// EndWithMsg finishes timing an event and logs an informational message.
+// Arguments are handled in the manner of fmt.Print.
+func (timing *Timing) EndWithMsg(v ...interface{}) time.Duration {
+	elapsed := time.Since(timing.started)
+	Infof("Finished %s | %s | %v elapsed", timing.msg, fmt.Sprint(v...), elapsed)
+	return elapsed
+}
+
+// EndWithMsgf finishes timing an event and logs an informational message.
+// Arguments are handled in the manner of fmt.Printf.
+func (timing *Timing) EndWithMsgf(format string, v ...interface{}) time.Duration {
+	elapsed := time.Since(timing.started)
+	Infof("Finished %s | %s | %v elapsed", timing.msg, fmt.Sprintf(format, v...), elapsed)
 	return elapsed
 }
 
