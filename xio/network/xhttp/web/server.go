c52bc1583a8cd6244548cc56786b661cecc8ee23
Richard Wilkes <wilkes@me.com>
2018-05-28 17:17:02 -0700

    Provide shutdown callback

--- xio/network/xhttp/web/server.go
+++ xio/network/xhttp/web/server.go
@@ -31,6 +31,7 @@ type Server struct {
 	ShutdownGracePeriod time.Duration
 	Logger              logadapter.Logger
 	WebServer           *http.Server
+	ShutdownCallback    func()
 	addresses           []string
 	port                int
 }
@@ -141,5 +142,8 @@ func (s *Server) Shutdown() {
 	defer cancel()
 	if err := s.WebServer.Shutdown(ctx); err != nil {
 		s.Logger.Warn(errs.NewfWithCause(err, "Unable to shutdown %s gracefully", s.Protocol()))
+	}
+	if s.ShutdownCallback != nil {
+		s.ShutdownCallback()
 	}
 }
