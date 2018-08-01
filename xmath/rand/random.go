557fccfc201c28430e1b5e736ca593a73c84f084
Richard Wilkes <wilkes@me.com>
2018-08-01 14:11:42 -0700

    Add convenience for accessing crypto/rand

---
+++ xmath/rand/random.go
@@ -0,0 +1,43 @@
+package rand
+
+import (
+	"crypto/rand"
+	mrnd "math/rand"
+)
+
+var cryptoRandInstance = &cryptoRand{}
+
+// NewCryptoRand returns a Randomizer based on the crypto/rand package.
+func NewCryptoRand() Randomizer {
+	return cryptoRandInstance
+}
+
+type cryptoRand struct {
+}
+
+func (r *cryptoRand) Intn(n int) int {
+	if n <= 0 {
+		return 0
+	}
+	var buffer [8]byte
+	size := 8
+	n64 := int64(n)
+	for i := 1; i < 8; i++ {
+		if n64 < int64(1)<<uint(i*8) {
+			size = i
+			break
+		}
+	}
+	if _, err := rand.Read(buffer[:size]); err != nil {
+		// Fallback to pseudo-random number generator if crypto/rand fails
+		return mrnd.Intn(n)
+	}
+	var v int
+	for i := size - 1; i >= 0; i-- {
+		v |= int(buffer[i]) << uint(i*8)
+	}
+	if v < 0 {
+		v = -v
+	}
+	return v % n
+}
