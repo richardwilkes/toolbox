f6eaa1a99a7f65a35112d7420c2fe4c1d7f50a40
Richard Wilkes <wilkes@me.com>
2018-10-14 18:13:48 -0700

    Add Align()

--- xmath/geom/point.go
+++ xmath/geom/point.go
@@ -2,11 +2,18 @@ package geom
 
 import (
 	"fmt"
+	"math"
 )
 
 // Point defines a location.
 type Point struct {
 	X, Y float64
+}
+
+// Align modifies this Point to align with integer coordinates.
+func (p *Point) Align() {
+	p.X = math.Floor(p.X)
+	p.Y = math.Floor(p.Y)
 }
 
 // Add modifies this Point by adding the supplied coordinates.
