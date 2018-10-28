4235422dc63edbb6e6d7dad24612137b69814f04
Richard Wilkes <wilkes@me.com>
2018-10-28 12:36:49 -0700

    Invert logic and change --log-only to --log-to-console

--- log/jotrotate/jotrotate.go
+++ log/jotrotate/jotrotate.go
@@ -17,17 +17,17 @@ func ParseAndSetup(cl *cmdline.CmdLine) []string {
 	logFile := rotation.DefaultPath()
 	var maxSize int64 = rotation.DefaultMaxSize
 	maxBackups := rotation.DefaultMaxBackups
-	quiet := false
+	logToConsole := false
 	cl.NewStringOption(&logFile).SetSingle('l').SetName("log-file").SetUsage("The file to write logs to")
 	cl.NewInt64Option(&maxSize).SetName("log-file-size").SetUsage("The maximum number of bytes to write to a log file before rotating it")
 	cl.NewIntOption(&maxBackups).SetName("log-file-backups").SetUsage("The maximum number of old logs files to retain")
-	cl.NewBoolOption(&quiet).SetSingle('q').SetName("log-only").SetUsage("Do not copy the log output to the console")
+	cl.NewBoolOption(&logToConsole).SetSingle('C').SetName("log-to-console").SetUsage("Copy the log output to the console")
 	remainingArgs := cl.Parse(os.Args[1:])
 	if rotator, err := rotation.New(rotation.Path(logFile), rotation.MaxSize(maxSize), rotation.MaxBackups(maxBackups)); err == nil {
-		if quiet {
-			jot.SetWriter(rotator)
-		} else {
+		if logToConsole {
 			jot.SetWriter(&xio.TeeWriter{Writers: []io.Writer{rotator, os.Stdout}})
+		} else {
+			jot.SetWriter(rotator)
 		}
 	} else {
 		jot.Error(err)
