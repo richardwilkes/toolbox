31960f14460014a62e17b4bde771d590576344c0
Richard Wilkes <wilkes@me.com>
2019-03-24 14:35:54 -0700

    Add NewSubFileSystem

---
+++ xio/fs/embedded/subfs.go
@@ -0,0 +1,48 @@
+package embedded
+
+import (
+	"net/http"
+	"path/filepath"
+)
+
+type subfs struct {
+	parent FileSystem
+	base   string
+}
+
+// NewSubFileSystem creates a new FileSystem rooted at 'base' within an
+// existing FileSystem.
+func NewSubFileSystem(parent FileSystem, base string) FileSystem {
+	return &subfs{
+		parent: parent,
+		base:   filepath.Clean("/" + base),
+	}
+}
+
+func (f *subfs) IsLive() bool {
+	return f.parent.IsLive()
+}
+
+func (f *subfs) adjustPath(path string) string {
+	return filepath.Join(f.base, filepath.Clean("/"+path))
+}
+
+func (f *subfs) Open(path string) (http.File, error) {
+	return f.parent.Open(f.adjustPath(path))
+}
+
+func (f *subfs) ContentAsBytes(path string) ([]byte, bool) {
+	return f.parent.ContentAsBytes(f.adjustPath(path))
+}
+
+func (f *subfs) MustContentAsBytes(path string) []byte {
+	return f.parent.MustContentAsBytes(f.adjustPath(path))
+}
+
+func (f *subfs) ContentAsString(path string) (string, bool) {
+	return f.parent.ContentAsString(f.adjustPath(path))
+}
+
+func (f *subfs) MustContentAsString(path string) string {
+	return f.parent.MustContentAsString(f.adjustPath(path))
+}
