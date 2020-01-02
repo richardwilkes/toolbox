a6ac3a03cc0ce6efec2b961775a689257d5218b6
Richard Wilkes <wilkes@me.com>
2020-01-02 15:59:49 -0800

    Updated build script

--- build.sh
+++ build.sh
@@ -1,42 +1,98 @@
-#!/bin/sh
+#! /usr/bin/env bash
 
-# This will install the various tools provided with this repo. The only
-# difference between running this and doing:
-#
-# go get -u github.com/richardwilkes/toolbox/...
-#
-# is proper version numbers with build dates and git revisions will be
-# embedded into the resulting executables.
+set -eo pipefail
 
-ROOT=`pwd`
+trap 'echo -e "\033[33;5mBuild failed on build.sh:$LINENO\033[0m"' ERR
 
-if which git 2>&1 > /dev/null; then
-    if [ -z "`git status --porcelain`" ]; then
-        STATE=clean
-    else
-        STATE=dirty
+RACE=-race
+
+# Process args
+for arg in "$@"
+do
+  case "$arg" in
+    --skip-linters|-l)    SKIP_LINTERS=1 ;;
+    --skip-test|-t)       SKIP_TESTS=1 ;;
+    --omit-race|-r)       RACE= ;;
+    --fast|-f)            SKIP_LINTERS=1; SKIP_TESTS=1 ;;
+    --help|-h)
+      echo "$0 [options]"
+      echo "  -f, --fast             Same as -l -t"
+      echo "  -l, --skip-linters     Skip linters"
+      echo "  -t, --skip-tests       Skip tests"
+      echo "  -r, --omit-race        Omit the -race option in tests"
+      echo "  -h, --help             This help text"
+      exit 0
+      ;;
+    *) echo "Invalid argument: $arg"; BAIL=1 ;;
+  esac
+done
+if [ -n "$BAIL" ]; then
+  exit 1
+fi
+
+# Setup the tools we'll need
+TOOLS_DIR=$PWD/tools
+mkdir -p "$TOOLS_DIR"
+if [ -z $SKIP_LINTERS ]; then
+  if [ ! -e "$TOOLS_DIR/golangci-lint" ] || [ "$("$TOOLS_DIR/golangci-lint" version 2>&1 | awk '{ print $4 }' || true)x" != "1.22.2x" ]; then
+    echo -e "\033[33mInstalling version 1.22.2 of golangci-lint into $TOOLS_DIR...\033[0m"
+    curl -sfL https://raw.githubusercontent.com/golangci/golangci-lint/master/install.sh | sh -s -- -b "$TOOLS_DIR" v1.22.2
+  fi
+fi
+export PATH=$TOOLS_DIR:$PATH
+
+# Setup version info
+if command -v git 2>&1 > /dev/null; then
+  if [ -z "$(git status --porcelain)" ]; then
+    STATE=clean
+  else
+    STATE=dirty
+  fi
+  GIT_VERSION=$(git rev-parse HEAD)-$STATE
+  GIT_TAG=$(git tag --points-at HEAD)
+  if [ -z $GIT_TAG ]; then
+    GIT_TAG=$(git tag --list --sort -version:refname | head -1)
+    if [ -n "$GIT_TAG" ]; then
+      GIT_TAG=$GIT_TAG~
     fi
-    GIT_VERSION=`git rev-parse HEAD`-$STATE
-    GIT_TAG=`git tag --points-at HEAD`
-    MAJOR=`echo $GIT_TAG | sed -E "s/v([0-9]+)\\.([0-9]+)\\.([0-9]+)/\1/"`
-    MINOR=`echo $GIT_TAG | sed -E "s/v([0-9]+)\\.([0-9]+)\\.([0-9]+)/\2/"`
-    PATCH=`echo $GIT_TAG | sed -E "s/v([0-9]+)\\.([0-9]+)\\.([0-9]+)/\3/"`
+  fi
+  if [ -n "$GIT_TAG" ]; then
+    VERSION=$(echo $GIT_TAG | sed -E "s/^v//")
+  else
+    VERSION=""
+  fi
 else
-    GIT_VERSION=Unknown
-fi
-if [ -z $MAJOR ]; then
-    MAJOR=0
-fi
-if [ -z $MINOR ]; then
-    MINOR=0
-fi
-if [ -z $PATCH ]; then
-    PATCH=0
+  GIT_VERSION=Unknown
+  VERSION=""
 fi
 
-go generate ./...
-
-VERSION=`go run cmd/genversion/main.go --major $MAJOR --minor $MINOR --patch $PATCH`
+# Build the code
+echo -e "\033[33mBuilding Go code...\033[0m"
 LINK_FLAGS="-X github.com/richardwilkes/toolbox/cmdline.AppVersion=$VERSION"
+LINK_FLAGS="$LINK_FLAGS -X github.com/richardwilkes/toolbox/cmdline.BuildNumber=$(date "+%Y%m%d%H%M%S")"
 LINK_FLAGS="$LINK_FLAGS -X github.com/richardwilkes/toolbox/cmdline.GitVersion=$GIT_VERSION"
-go install -v -ldflags=all="$LINK_FLAGS" ./...
+LINK_FLAGS="$LINK_FLAGS -X github.com/richardwilkes/toolbox/cmdline.CopyrightYears=2016-$(date "+%Y")"
+find . -iname "*_gen.go" -exec /bin/rm {} \;
+go generate ./...
+go build -v -ldflags=all="$LINK_FLAGS" ./...
+
+# Run the linters
+if [ -z $SKIP_LINTERS ]; then
+  echo -e "\033[33mRunning Go linters...\033[0m"
+  golangci-lint run
+else
+  echo -e "\033[33mSkipping Go linters\033[0m"
+fi
+
+# Run the tests
+if [ -z $SKIP_TESTS ]; then
+  echo -e "\033[33mRunning tests...\033[0m"
+  go test $RACE ./...
+else
+  echo -e "\033[33mSkipping tests\033[0m"
+fi
+
+# Install executables
+echo -e "\033[33mInstalling executables...\033[0m"
+go install -ldflags=all="$LINK_FLAGS" ./xio/fs/mkembeddedfs
+go install -ldflags=all="$LINK_FLAGS" ./i18n/i18n
