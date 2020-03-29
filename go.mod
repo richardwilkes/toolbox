073caf7badf3be79db1c3230ef672ca48399f4f1
Richard Wilkes <wilkes@me.com>
2020-03-29 16:23:06 -0700

    Add fs.UniquePaths()

--- go.mod
+++ go.mod
@@ -4,9 +4,10 @@ go 1.14
 
 require (
 	github.com/dustin/go-humanize v1.0.0
-	github.com/jackpal/gateway v1.0.5
+	github.com/jackpal/gateway v1.0.6
 	github.com/pkg/term v0.0.0-20190109203006-aa71e9d9e942
 	github.com/stretchr/testify v1.5.1
-	golang.org/x/sys v0.0.0-20200321134203-328b4cd54aae // indirect
+	github.com/yookoala/realpath v1.0.0
+	golang.org/x/sys v0.0.0-20200327173247-9dae0f8f5775 // indirect
 	gopkg.in/yaml.v2 v2.2.8
 )
---
+++ xio/fs/roots.go
@@ -0,0 +1,59 @@
+// Copyright ©2016-2020 by Richard A. Wilkes. All rights reserved.
+//
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, version 2.0. If a copy of the MPL was not distributed with
+// this file, You can obtain one at http://mozilla.org/MPL/2.0/.
+//
+// This Source Code Form is "Incompatible With Secondary Licenses", as
+// defined by the Mozilla Public License, version 2.0.
+
+package fs
+
+import (
+	"path/filepath"
+	"strings"
+
+	"github.com/richardwilkes/toolbox/errs"
+	"github.com/yookoala/realpath"
+)
+
+// UniquePaths returns a list of unique paths from the given paths, pruning
+// out paths that are a subset of another.
+func UniquePaths(paths ...string) ([]string, error) {
+	set := make(map[string]bool)
+	for _, path := range paths {
+		actual, err := realpath.Realpath(path)
+		if err != nil {
+			return nil, errs.NewWithCause(path, err)
+		}
+		if _, exists := set[actual]; !exists {
+			add := true
+			for one := range set {
+				var p1, p2 string
+				if p1, err = filepath.Rel(one, actual); err != nil {
+					return nil, errs.NewWithCause(path, err)
+				}
+				if p2, err = filepath.Rel(actual, one); err != nil {
+					return nil, errs.NewWithCause(path, err)
+				}
+				prefixed := strings.HasPrefix(p1, "..")
+				if prefixed != strings.HasPrefix(p2, "..") {
+					if prefixed {
+						delete(set, one)
+					} else {
+						add = false
+						break
+					}
+				}
+			}
+			if add {
+				set[actual] = true
+			}
+		}
+	}
+	result := make([]string, 0, len(set))
+	for p := range set {
+		result = append(result, p)
+	}
+	return result, nil
+}
