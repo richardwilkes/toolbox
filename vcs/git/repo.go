c4a3842cea6aa979a8fcbab0fbb44c15c8802538
Richard Wilkes <wilkes@me.com>
2021-07-30 21:35:24 -0700

    Update copyright notices

--- vcs/git/repo.go
+++ vcs/git/repo.go
@@ -1,4 +1,4 @@
-// Copyright ©2016-2020 by Richard A. Wilkes. All rights reserved.
+// Copyright ©2016-2021 by Richard A. Wilkes. All rights reserved.
 //
 // This Source Code Form is subject to the terms of the Mozilla Public
 // License, version 2.0. If a copy of the MPL was not distributed with
@@ -25,8 +25,8 @@ import (
 )
 
 var (
-	tagsRefListRegex   = regexp.MustCompile(`(?m)(?:tags)/(\S+)$`)
-	originRefListRegex = regexp.MustCompile(`(?m)(?:origin)/(\S+)$`)
+	tagsRefListRegex   = regexp.MustCompile(`(?m)tags/(\S+)$`)
+	originRefListRegex = regexp.MustCompile(`(?m)origin/(\S+)$`)
 )
 
 // Repo provides access to a git repository.
