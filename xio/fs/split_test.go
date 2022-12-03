d2961bca8adc24dac46e12a86e97a1f22cb75b07
Richard Wilkes <wilkes@me.com>
2022-12-02 19:35:57 -0800

    Split platform-specific testing into its own file

--- xio/fs/split_test.go
+++ xio/fs/split_test.go
@@ -11,10 +11,8 @@ package fs_test
 
 import (
 	"path/filepath"
-	"runtime"
 	"testing"
 
-	"github.com/richardwilkes/toolbox"
 	"github.com/richardwilkes/toolbox/xio/fs"
 	"github.com/stretchr/testify/assert"
 )
@@ -59,18 +57,6 @@ func TestSplit(t *testing.T) {
 			in:  "/one/../..//two.txt/.",
 			out: []string{full, "two.txt"},
 		},
-	}
-	if runtime.GOOS == toolbox.WindowsOS {
-		data = append(data,
-			splitData{
-				in:  `C:\one/two.txt`,
-				out: []string{`C:\`, "one", "two.txt"},
-			},
-			splitData{
-				in:  `\\host\share\one\two.txt`,
-				out: []string{`\\host\share\`, "one", "two.txt"},
-			},
-		)
 	}
 	for i, one := range data {
 		assert.Equal(t, one.out, fs.Split(one.in), "%d", i)
---
+++ xio/fs/split_windows_test.go
@@ -0,0 +1,33 @@
+// Copyright ©2016-2022 by Richard A. Wilkes. All rights reserved.
+//
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, version 2.0. If a copy of the MPL was not distributed with
+// this file, You can obtain one at http://mozilla.org/MPL/2.0/.
+//
+// This Source Code Form is "Incompatible With Secondary Licenses", as
+// defined by the Mozilla Public License, version 2.0.
+
+package fs_test
+
+import (
+	"testing"
+
+	"github.com/richardwilkes/toolbox/xio/fs"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestWindowsSplit(t *testing.T) {
+	data := []splitData{
+		{
+			in:  `C:\one/two.txt`,
+			out: []string{`C:\`, "one", "two.txt"},
+		},
+		{
+			in:  `\\host\share\one\two.txt`,
+			out: []string{`\\host\share\`, "one", "two.txt"},
+		},
+	}
+	for i, one := range data {
+		assert.Equal(t, one.out, fs.Split(one.in), "%d", i)
+	}
+}
