8cff0981939960ee1a5a5d5616ea90bd7e60408c
Richard Wilkes <wilkes@me.com>
2022-12-02 23:26:37 -0800

    Added slice.ZeroedDelete

---
+++ collection/slice/slice.go
@@ -0,0 +1,25 @@
+// Copyright ©2016-2022 by Richard A. Wilkes. All rights reserved.
+//
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, version 2.0. If a copy of the MPL was not distributed with
+// this file, You can obtain one at http://mozilla.org/MPL/2.0/.
+//
+// This Source Code Form is "Incompatible With Secondary Licenses", as
+// defined by the Mozilla Public License, version 2.0.
+
+package slice
+
+// ZeroedDelete removes the elements s[i:j] from s, returning the modified slice. This function panics if s[i:j] is not
+// a valid slice of s. This function modifies the contents of the slice s -- it does not create a new slice. The
+// elements that are removed are zeroed so that any references can be garbage collected. If you do not need this, use
+// slices.Delete from the golang.org/x/exp package instead.
+func ZeroedDelete[S ~[]E, E any](s S, i, j int) S {
+	_ = s[i:j] // bounds check
+	copy(s[i:], s[j:])
+	var e E
+	end := len(s) - j + i
+	for k, n := end, len(s); k < n; k++ {
+		s[k] = e
+	}
+	return s[:end]
+}
---
+++ collection/slice/slice_test.go
@@ -0,0 +1,69 @@
+// Copyright ©2016-2022 by Richard A. Wilkes. All rights reserved.
+//
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, version 2.0. If a copy of the MPL was not distributed with
+// this file, You can obtain one at http://mozilla.org/MPL/2.0/.
+//
+// This Source Code Form is "Incompatible With Secondary Licenses", as
+// defined by the Mozilla Public License, version 2.0.
+
+package slice_test
+
+import (
+	"testing"
+
+	"github.com/richardwilkes/toolbox/collection/slice"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestZeroedDelete(t *testing.T) {
+	type data struct {
+		a int
+	}
+	d1 := &data{a: 1}
+	d2 := &data{a: 2}
+	d3 := &data{a: 3}
+	for _, test := range []struct {
+		s    []*data
+		i, j int
+		want []*data
+	}{
+		{
+			[]*data{d1, d2, d3},
+			0,
+			0,
+			[]*data{d1, d2, d3},
+		},
+		{
+			[]*data{d1, d2, d3},
+			0,
+			1,
+			[]*data{d2, d3},
+		},
+		{
+			[]*data{d1, d2, d3},
+			3,
+			3,
+			[]*data{d1, d2, d3},
+		},
+		{
+			[]*data{d1, d2, d3},
+			0,
+			2,
+			[]*data{d3},
+		},
+		{
+			[]*data{d1, d2, d3},
+			0,
+			3,
+			[]*data{},
+		},
+	} {
+		theCopy := append([]*data{}, test.s...)
+		result := slice.ZeroedDelete(theCopy, test.i, test.j)
+		assert.Equal(t, result, test.want, "ZeroedDelete(%v, %d, %d) = %v, want %v", test.s, test.i, test.j, result, test.want)
+		for i := len(result); i < len(theCopy); i++ {
+			assert.Nil(t, theCopy[i], "residual element %d should have been nil, was %v", i, theCopy[i])
+		}
+	}
+}
