3017015bfc6be0f80dc8ec62de4da17459c1c225
Richard Wilkes <wilkes@me.com>
2023-01-02 19:12:00 -0800

    Fix missing imports for other OS's

--- desktop/browser_linux.go
+++ desktop/browser_linux.go
@@ -10,6 +10,12 @@
 // Package desktop provides desktop integration utilities.
 package desktop
 
+import (
+	"os/exec"
+
+	"github.com/richardwilkes/toolbox/errs"
+)
+
 // Open asks the system to open the provided path or URL.
 func Open(pathOrURL string) error {
 	if err := exec.Command("xdg-open", pathOrURL).Start(); err != nil {
