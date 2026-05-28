88da212a4fd9fc70cbec0d4b4ce636665dbab49b
Richard Wilkes <wilkes@me.com>
2026-05-28 11:25:52 -0700

    uti.Register now returns the DataType

--- uti/datatype.go
+++ uti/datatype.go
@@ -15,37 +15,127 @@ import (
 	"sync"
 )
 
-// Some well-known Uniform Type Identifiers
-var (
-	BMP           *DataType
-	Content       *DataType
-	Data          *DataType
-	FileURL       *DataType
-	GIF           *DataType
-	ICNS          *DataType
-	ICO           *DataType
-	Image         *DataType
-	Item          *DataType
-	JPEG          *DataType
-	JSON          *DataType
-	PlainText     *DataType
-	PNG           *DataType
-	SVG           *DataType
-	Text          *DataType
-	TIFF          *DataType
-	URL           *DataType
-	UTF8PlainText *DataType
-	WBMP          *DataType
-	WEBP          *DataType
-	XML           *DataType
-	YAML          *DataType
-)
-
 var (
 	lock        sync.RWMutex
 	byUTI       = make(map[string]*DataType)
 	byMimeType  = make(map[string][]*DataType)
 	byExtension = make(map[string][]*DataType)
+)
+
+// Some well-known Uniform Type Identifiers
+var (
+	Content = Register(&DataType{UTI: "public.content"})
+	Item    = Register(&DataType{UTI: "public.item"})
+	Data    = Register(&DataType{
+		UTI:       "public.data",
+		Parents:   []*DataType{Item},
+		MimeTypes: []string{"application/octet-stream"},
+	})
+	Image = Register(&DataType{
+		UTI:     "public.image",
+		Parents: []*DataType{Data, Content},
+	})
+	Text = Register(&DataType{
+		UTI:     "public.text",
+		Parents: []*DataType{Data, Content},
+	})
+	PlainText = Register(&DataType{
+		UTI:        "public.plain-text",
+		Parents:    []*DataType{Text},
+		MimeTypes:  []string{"text/plain"},
+		Extensions: []string{".txt", ".text"},
+	})
+	UTF8PlainText = Register(&DataType{
+		UTI:       "public.utf8-plain-text",
+		Parents:   []*DataType{PlainText},
+		MimeTypes: []string{"text/plain;charset=utf-8", `text/plain;charset="utf-8"`},
+	})
+	JSON = Register(&DataType{
+		UTI:        "public.json",
+		Parents:    []*DataType{Text},
+		MimeTypes:  []string{"application/json"},
+		Extensions: []string{".json"},
+	})
+	XML = Register(&DataType{
+		UTI:        "public.xml",
+		Parents:    []*DataType{Text},
+		MimeTypes:  []string{"text/xml", "application/xml"},
+		Extensions: []string{".xml"},
+	})
+	YAML = Register(&DataType{
+		UTI:        "public.yaml",
+		Parents:    []*DataType{Text},
+		MimeTypes:  []string{"application/x-yaml"},
+		Extensions: []string{".yaml", ".yml"},
+	})
+
+	BMP = Register(&DataType{
+		UTI:        "com.microsoft.bmp",
+		Parents:    []*DataType{Image},
+		MimeTypes:  []string{"image/bmp", "image/x-bmp"},
+		Extensions: []string{".bmp", ".dib"},
+	})
+	GIF = Register(&DataType{
+		UTI:        "com.compuserve.gif",
+		Parents:    []*DataType{Image},
+		MimeTypes:  []string{"image/gif"},
+		Extensions: []string{".gif"},
+	})
+	ICNS = Register(&DataType{
+		UTI:        "com.apple.icns",
+		Parents:    []*DataType{Image},
+		Extensions: []string{".icns"},
+	})
+	ICO = Register(&DataType{
+		UTI:        "com.microsoft.ico",
+		Parents:    []*DataType{Image},
+		MimeTypes:  []string{"image/x-icon", "image/vnd.microsoft.icon"},
+		Extensions: []string{".ico"},
+	})
+	JPEG = Register(&DataType{
+		UTI:        "public.jpeg",
+		Parents:    []*DataType{Image},
+		MimeTypes:  []string{"image/jpeg", "image/jpg"},
+		Extensions: []string{"jpeg", ".jpg", "jpe", ".jif", ".jfif", ".jfi"},
+	})
+	PNG = Register(&DataType{
+		UTI:        "public.png",
+		Parents:    []*DataType{Image},
+		MimeTypes:  []string{"image/png"},
+		Extensions: []string{".png"},
+	})
+	SVG = Register(&DataType{
+		UTI:        "public.svg-image",
+		Parents:    []*DataType{Image, XML},
+		MimeTypes:  []string{"image/svg+xml"},
+		Extensions: []string{".svg", ".svgz"},
+	})
+	TIFF = Register(&DataType{
+		UTI:        "public.tiff",
+		Parents:    []*DataType{Image},
+		MimeTypes:  []string{"image/tiff"},
+		Extensions: []string{".tiff", ".tif"},
+	})
+	WBMP = Register(&DataType{
+		UTI:        "com.adobe.wbmp",
+		Parents:    []*DataType{Image},
+		MimeTypes:  []string{"image/vnd.wap.wbmp"},
+		Extensions: []string{".wbmp"},
+	})
+	WEBP = Register(&DataType{
+		UTI:        "org.webmproject.webp",
+		Parents:    []*DataType{Image},
+		MimeTypes:  []string{"image/webp"},
+		Extensions: []string{".webp"},
+	})
+	URL = Register(&DataType{
+		UTI:     "public.url",
+		Parents: []*DataType{Data},
+	})
+	FileURL = Register(&DataType{
+		UTI:     "public.file-url",
+		Parents: []*DataType{URL},
+	})
 )
 
 // DataType holds information about a single Uniform Type Identifier (UTI) and its relationship to other UTI's, MIME
@@ -58,129 +148,9 @@ type DataType struct {
 	Extensions []string
 }
 
-func init() {
-	Content = register(&DataType{UTI: "public.content"})
-	Item = register(&DataType{UTI: "public.item"})
-	Data = register(&DataType{
-		UTI:       "public.data",
-		Parents:   []*DataType{Item},
-		MimeTypes: []string{"application/octet-stream"},
-	})
-	Image = register(&DataType{
-		UTI:     "public.image",
-		Parents: []*DataType{Data, Content},
-	})
-	Text = register(&DataType{
-		UTI:     "public.text",
-		Parents: []*DataType{Data, Content},
-	})
-	PlainText = register(&DataType{
-		UTI:        "public.plain-text",
-		Parents:    []*DataType{Text},
-		MimeTypes:  []string{"text/plain"},
-		Extensions: []string{".txt", ".text"},
-	})
-	UTF8PlainText = register(&DataType{
-		UTI:       "public.utf8-plain-text",
-		Parents:   []*DataType{PlainText},
-		MimeTypes: []string{"text/plain;charset=utf-8", `text/plain;charset="utf-8"`},
-	})
-	JSON = register(&DataType{
-		UTI:        "public.json",
-		Parents:    []*DataType{Text},
-		MimeTypes:  []string{"application/json"},
-		Extensions: []string{".json"},
-	})
-	XML = register(&DataType{
-		UTI:        "public.xml",
-		Parents:    []*DataType{Text},
-		MimeTypes:  []string{"text/xml", "application/xml"},
-		Extensions: []string{".xml"},
-	})
-	YAML = register(&DataType{
-		UTI:        "public.yaml",
-		Parents:    []*DataType{Text},
-		MimeTypes:  []string{"application/x-yaml"},
-		Extensions: []string{".yaml", ".yml"},
-	})
-
-	BMP = register(&DataType{
-		UTI:        "com.microsoft.bmp",
-		Parents:    []*DataType{Image},
-		MimeTypes:  []string{"image/bmp", "image/x-bmp"},
-		Extensions: []string{".bmp", ".dib"},
-	})
-	GIF = register(&DataType{
-		UTI:        "com.compuserve.gif",
-		Parents:    []*DataType{Image},
-		MimeTypes:  []string{"image/gif"},
-		Extensions: []string{".gif"},
-	})
-	ICNS = register(&DataType{
-		UTI:        "com.apple.icns",
-		Parents:    []*DataType{Image},
-		Extensions: []string{".icns"},
-	})
-	ICO = register(&DataType{
-		UTI:        "com.microsoft.ico",
-		Parents:    []*DataType{Image},
-		MimeTypes:  []string{"image/x-icon", "image/vnd.microsoft.icon"},
-		Extensions: []string{".ico"},
-	})
-	JPEG = register(&DataType{
-		UTI:        "public.jpeg",
-		Parents:    []*DataType{Image},
-		MimeTypes:  []string{"image/jpeg", "image/jpg"},
-		Extensions: []string{"jpeg", ".jpg", "jpe", ".jif", ".jfif", ".jfi"},
-	})
-	PNG = register(&DataType{
-		UTI:        "public.png",
-		Parents:    []*DataType{Image},
-		MimeTypes:  []string{"image/png"},
-		Extensions: []string{".png"},
-	})
-	SVG = register(&DataType{
-		UTI:        "public.svg-image",
-		Parents:    []*DataType{Image, XML},
-		MimeTypes:  []string{"image/svg+xml"},
-		Extensions: []string{".svg", ".svgz"},
-	})
-	TIFF = register(&DataType{
-		UTI:        "public.tiff",
-		Parents:    []*DataType{Image},
-		MimeTypes:  []string{"image/tiff"},
-		Extensions: []string{".tiff", ".tif"},
-	})
-	WBMP = register(&DataType{
-		UTI:        "com.adobe.wbmp",
-		Parents:    []*DataType{Image},
-		MimeTypes:  []string{"image/vnd.wap.wbmp"},
-		Extensions: []string{".wbmp"},
-	})
-	WEBP = register(&DataType{
-		UTI:        "org.webmproject.webp",
-		Parents:    []*DataType{Image},
-		MimeTypes:  []string{"image/webp"},
-		Extensions: []string{".webp"},
-	})
-	URL = register(&DataType{
-		UTI:     "public.url",
-		Parents: []*DataType{Data},
-	})
-	FileURL = register(&DataType{
-		UTI:     "public.file-url",
-		Parents: []*DataType{URL},
-	})
-}
-
-func register(dataType *DataType) *DataType {
-	Register(dataType)
-	return dataType
-}
-
 // Register the DataType. If the UTI has been previously registered, the old one will first be unregistered and then the
 // new one will be registered.
-func Register(dataType *DataType) {
+func Register(dataType *DataType) *DataType {
 	uti := strings.ToLower(dataType.UTI)
 	lock.Lock()
 	defer lock.Unlock()
@@ -194,6 +164,7 @@ func Register(dataType *DataType) {
 		extension = strings.ToLower(extension)
 		byExtension[extension] = append(byExtension[extension], dataType)
 	}
+	return dataType
 }
 
 // Unregister the DataType.
